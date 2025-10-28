2f9e8d93e4bbf51bfba5902609d5346cf97157c1
OZAWA Sakuro <10973+sakuro@users.noreply.github.com>
2025-10-28 14:29:04 +0900

    :package: Add zeitwerk for autoloading

--- lib/wikiwiki.rb
+++ lib/wikiwiki.rb
@@ -1,8 +1,12 @@
 # frozen_string_literal: true
 
+require "zeitwerk"
 require_relative "wikiwiki/version"
 
 module Wikiwiki
   class Error < StandardError; end
-  # Your code goes here...
+
+  loader = Zeitwerk::Loader.for_gem
+  loader.setup
+  loader.eager_load
 end
